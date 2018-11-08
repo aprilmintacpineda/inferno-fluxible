89eeb812681cfab4f8c842f74985e10ae8fc93c1
April Mintac Pineda <aprilmintacpineda@gmail.com>
2018-11-08 23:26:16 +0800

    removed connect and dispatch

--- example/src/lib.js
+++ example/src/lib.js
@@ -4,8 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.mapStatesToProps = mapStatesToProps;
-exports.dispatch = dispatch;
-exports.connect = connect;
 
 var _inferno = require("inferno");
 
@@ -19,7 +17,7 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-function mapStatesToProps(TargetComponent, callback) {
+function mapStatesToProps(WrappedComponent, callback) {
   function ConnectedComponent(props) {
     var _this = this;
 
@@ -37,7 +35,7 @@ function mapStatesToProps(TargetComponent, callback) {
     }, Object.keys(mappedStates));
 
     this.render = function () {
-      return (0, _inferno.normalizeProps)((0, _inferno.createComponentVNode)(2, TargetComponent, _objectSpread({}, _this.props, mappedStates)));
+      return (0, _inferno.normalizeProps)((0, _inferno.createComponentVNode)(2, WrappedComponent, _objectSpread({}, _this.props, mappedStates)));
     };
 
     return this;
@@ -45,108 +43,6 @@ function mapStatesToProps(TargetComponent, callback) {
 
   ConnectedComponent.prototype = _inferno.Component.prototype;
   ConnectedComponent.prototype.constructor = ConnectedComponent;
+  (0, _redefineStaticsJs.default)(ConnectedComponent, WrappedComponent);
   return ConnectedComponent;
-}
-/*
- * NOTICE:
- * FUNCTIONS BELOW ARE DEPRECATED AND WILL BE REMOVED IN THE NEXT MAJOR RELEASE.
- */
-
-/**
- * @param {Function} callback function that would be called sa the mutation handler.
- * @param  {...any} payload to the callback function
- */
-
-
-function dispatch(mutation) {
-  for (var _len = arguments.length, payload = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-    payload[_key - 1] = arguments[_key];
-  }
-
-  return mutation.apply(void 0, [{
-    getStore: _fluxibleJs.getStore,
-    updateStore: function updateStore(updatedState, callback) {
-      (0, _fluxibleJs.updateStore)(updatedState);
-
-      if (callback) {
-        callback();
-      }
-    }
-  }].concat(payload));
-}
-/**
- * @param {Function} Would receive the current store's state as the only argument. Should return an object of the states that you want to be accessible in the connected component.
- * @param {Object} You should define your action handlers here. Each methods would be called with an object (that has `updateStore` and `getStore` methods) as the first argument. The rest would be the arguments you passed to the call.
- * @return {Object} the inferno component.
- */
-
-
-function connect(mapStatesToProps, definedMutations) {
-  return function (WrappedComponent) {
-    // we only want to compute mutations once
-    var mutations = {};
-
-    if (definedMutations) {
-      (function () {
-        var mutationKeys = Object.keys(definedMutations);
-
-        var _loop = function _loop(a) {
-          mutations[mutationKeys[a]] = function () {
-            for (var _len2 = arguments.length, payload = new Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
-              payload[_key2] = arguments[_key2];
-            }
-
-            return dispatch.apply(void 0, [definedMutations[mutationKeys[a]]].concat(payload));
-          };
-        };
-
-        for (var a = 0; a < mutationKeys.length; a++) {
-          _loop(a);
-        }
-      })();
-    }
-
-    function ConnectedComponent(props) {
-      var _this2 = this;
-
-      this.props = props;
-
-      if (mapStatesToProps) {
-        this.mappedStates = mapStatesToProps((0, _fluxibleJs.getStore)());
-        this.removeListener = (0, _fluxibleJs.addObserver)(function () {
-          _this2.mappedStates = mapStatesToProps((0, _fluxibleJs.getStore)());
-
-          _this2.setState({
-            count: _this2.state.count + 1
-          });
-        }, Object.keys(this.mappedStates));
-        this.state = {
-          count: 0
-        };
-      }
-
-      this.componentWillUnmount = function () {
-        if (_this2.removeListener) {
-          _this2.removeListener();
-        }
-      };
-
-      this.render = function () {
-        if (mapStatesToProps && definedMutations) {
-          return (0, _inferno.normalizeProps)((0, _inferno.createComponentVNode)(2, WrappedComponent, _objectSpread({}, _this2.props, mutations, _this2.mappedStates)));
-        } else if (mapStatesToProps) {
-          return (0, _inferno.normalizeProps)((0, _inferno.createComponentVNode)(2, WrappedComponent, _objectSpread({}, _this2.props, _this2.mappedStates)));
-        }
-
-        return (0, _inferno.normalizeProps)((0, _inferno.createComponentVNode)(2, WrappedComponent, _objectSpread({}, _this2.props, mutations)));
-      };
-
-      return this;
-    }
-
-    ConnectedComponent.prototype = _inferno.Component.prototype;
-    ConnectedComponent.prototype.constructor = ConnectedComponent;
-    (0, _redefineStaticsJs.default)(ConnectedComponent, WrappedComponent);
-    return ConnectedComponent;
-  };
 }
